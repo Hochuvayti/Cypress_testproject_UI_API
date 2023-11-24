b13279c3087ccc24d9e6fa8849ac59a8034fd6ab
Hochuvayti <nabukve@gmail.com>
2023-11-24 22:01:14 +0200

    add alt

--- cypress/e2e/spec_4HW_API.cy.js
+++ cypress/e2e/spec_4HW_API.cy.js
@@ -1,5 +1,5 @@
 describe('api testing', () => {
-    it('method get response code should be 200', () => {
+    it('method get response code should be  200', () => {
       cy.request('https://httpbin.org/').then(response => {
         const status = response.status;
         assert.equal(200, status)
